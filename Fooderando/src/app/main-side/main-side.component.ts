24858a74c50111fb0156aa681bc2ae862efe54bd
Robin Osburg <Robin.osburg@web.de>
2023-02-10 11:49:31 +0100

    start

---
+++ Fooderando/src/app/main-side/main-side.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-main-side',
+  templateUrl: './main-side.component.html',
+  styleUrls: ['./main-side.component.scss']
+})
+export class MainSideComponent {
+
+}
