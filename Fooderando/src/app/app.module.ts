944e60b2fffebc3cfcc821acbf08ba1ff831b661
Robin Osburg <robin.osburg@web.de>
2023-02-23 15:05:16 +0100

    added some components and first image

--- Fooderando/src/app/app.module.ts
+++ Fooderando/src/app/app.module.ts
@@ -4,11 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainSideComponent } from './main-side/main-side.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { BasketComponent } from './basket/basket.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MainSideComponent
+    MainSideComponent,
+    HeaderComponent,
+    FooterComponent,
+    BasketComponent
   ],
   imports: [
     BrowserModule,
